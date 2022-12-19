a816cd3c056996207e849428ec563e76a3b2cdb2
Aleksandr Levus <aleksandr.l@ati.su>
2022-12-19 14:46:43 +0800

    Use Brotli instead Gzip

--- src/zip.js
+++ src/zip.js
@@ -2,17 +2,19 @@ import zlib from "node:zlib";
 import { createReadStream, createWriteStream } from "node:fs";
 
 const compress = async (pathToFile, pathToDest) => {
-    const zip = zlib.createGzip();
+    const zip = zlib.createBrotliCompress();
+
     const read = createReadStream(pathToFile);
     const write = createWriteStream(pathToDest);
 
     read.pipe(zip).pipe(write);
 };
 
-const decompress = async (pathToFile, pathToDest) => {
-    const zip = zlib.createGunzip();
-    const read = createReadStream(pathToDest);
-    const write = createWriteStream(pathToFile);
+const decompress = async (pathToArchive, pathToDest) => {
+    const zip = zlib.createBrotliDecompress();
+
+    const read = createReadStream(pathToArchive);
+    const write = createWriteStream(pathToDest);
 
     read.pipe(zip).pipe(write);
 };
